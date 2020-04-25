b55a36bd38b95e618d9cea19e5db21edac06f28a
slushercj <slusher.cj@gmail.com>
2020-04-25 08:34:45 -0700

    splash screen

---
+++ BathroomBreak/Configuration/BathroomBreakConstants.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace BathroomBreak.Configuration
+{
+    public static class BathroomBreakConstants
+    {
+        public static int SplashScreenTimeout { get { return 2500; } }
+    }
+}
---
+++ BathroomBreak/Configuration/Constants.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace BathroomBreak.Constants
+{
+    public static class BathroomBreakConstants
+    {
+        public static int SplashScreenTimeout { get { return 2500; } }
+    }
+}
